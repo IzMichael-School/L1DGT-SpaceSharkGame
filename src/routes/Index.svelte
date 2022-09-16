7a9571574617089bc8e403fb4a131253717090fc
IzMichael <themichael77@outlook.com>
2022-09-17 00:25:14 +1200

    Add music, high scores, menu interface improvements

--- src/routes/Index.svelte
+++ src/routes/Index.svelte
@@ -10,8 +10,10 @@
         state = 'menu',
         killCanvas = false,
         game,
+        music,
         timer,
         timerVal = 0,
+        highscore = window.localStorage.getItem('highscore') || 0,
         config = {},
         assets = [],
         centre = { x: 0, y: 0 },
@@ -24,6 +26,8 @@
             D: undefined,
             W: undefined
         };
+
+    $: window.localStorage.setItem('highscore', highscore);
 
     function loadGame() {
         endGame();
@@ -74,20 +78,25 @@
             frameHeight: 30
         });
         this.load.image('earth', 'assets/img/earth2.png');
-        this.load.image('asteroid', 'assets/img/asteroid.png');
+        // this.load.image('asteroid', 'assets/img/asteroid.png');
+        this.load.spritesheet('asteroid', 'assets/img/asteroid-sheet.png', {
+            frameWidth: 1040/8,
+            frameHeight: 130
+        });
         this.load.image('star', 'assets/img/star.png');
 
-        this.load.audio('bgm1', 'assets/audio/salted-fanfare_dans_lusine.mp3');
+        this.load.audio('fanfare_dans_lusine', 'assets/audio/salted-fanfare_dans_lusine.mp3');
+        this.load.audio('castles_wars', 'assets/audio/salted-castles_wars.mp3');
+        this.load.audio('traps', 'assets/audio/salted-traps.mp3');
+        this.load.audio('burglar_theme', 'assets/audio/salted-burglar_theme.mp3');
     };
 
     function create() {
         // Music
-        // this.music = this.sound.add('bgm1', {
-        //     volume: 0.2,
-        //     loop: true
-        // });
-
-        // this.music.play();
+        // Play first song
+        playBgm();
+        // Detect song end, play next song
+        music.on('complete', () => playBgm());
         
         // Create sprites, keyboard listeners, and text displays
         
@@ -110,6 +119,17 @@
             frames: this.anims.generateFrameNumbers('shark', {
                 start: 0,
                 end: 1
+            }),
+            frameRate: 10,
+            repeat: 1
+        });
+
+        // The asteroid explosion animation
+        this.anims.create({
+            key: 'asteroid_explosion',
+            frames: this.anims.generateFrameNumbers('asteroid', {
+                start: 0,
+                end: 8
             }),
             frameRate: 10,
             repeat: 1
@@ -155,6 +175,7 @@
             let asteroid = assets.asteroids.create((Math.random() * (0 - config.width + 1) + config.width), -500, 'asteroid');
             asteroid.angle = Math.random() * (360 - 0 + 1) + 0;
             this.physics.moveTo(asteroid, (Math.random() * (0 - (config.width / 2) + 1) + (config.width / 2)) + (config.width / 2), config.height, Math.random() * (200 - 25 + 1) + 25);
+            // asteroid.anims.play('asteroid_move', true);
         // }, Math.random() * (7500 - (-7500) + 1000) + -7500);
         }, 5000);
 
@@ -162,44 +183,57 @@
             let star = assets.stars.create(-500, (Math.random() * (0 - (config.height) + 1) + config.height), 'star');
             star.angle = Math.random() * (360 - 0 + 1) + 0;
             this.physics.moveTo(star, config.width, (Math.random() * (0 - (config.height) + 1) + config.height), Math.random() * (200 - 25 + 1) + 25);
+            // star.anims.play('star_move', true);
             // star.body.collideWorldBounds = true;
             // star.body.bounce.set(1);
         // }, Math.random() * (7500 - (-7500) + 1000) + -7500);
         }, 2000);
 
         // Add HP indicators
-        assets.sharkhp = this.add.text(10, 10, 'HP: ' + player.hp + '%').setFontFamily('Nunito Sans').setFontSize(16);
+        assets.sharkhp = this.add.text(10, 10, 'HP: ' + player.hp + '%').setFontFamily('Overfield').setFontSize(16);
         player.hp = 100;
         
-        assets.earthhp = this.add.text(centre.x, config.height - 60, 'HP: ' + earth.hp + '%').setFontFamily('Nunito Sans').setFontSize(32);
+        assets.earthhp = this.add.text(centre.x, config.height - 60, 'HP: ' + earth.hp + '%').setFontFamily('Overfield').setFontSize(32);
         earth.hp = 100;
 
         // Add Elapsed Time Display
-        assets.timer = this.add.text(10, 10, formatSecs(timerVal)).setFontFamily('Nunito Sans').setFontSize(32);
+        assets.timer = this.add.text(10, 10, formatSecs(timerVal)).setFontFamily('Overfield').setFontSize(32);
 
         // Add Collision Functions
         this.time.delayedCall(1000, () => {
             // When Player collides with asteroid, reduce Player HP and delete asteroid
-            this.physics.add.overlap(player, assets.asteroids, (player, asteroid) => {
-                if (!player.hp >= 0) {
-                    player.hp -= 10;
-                };
+            this.physics.add.overlap(player, assets.asteroids, async (player, asteroid) => {
+                player.hp -= 10;
+                // asteroid.anims.play('asteroid_explosion', true);
+                // await sleep(5)
                 asteroid.disableBody(true, true);
             }, null, this);
             
             // When Player collides with star, increase Player HP and delete star
-            this.physics.add.overlap(player, assets.stars, (player, star) => {
-                if (!player.hp <= 100) {
-                    player.hp += 5;
-                };
+            this.physics.add.overlap(player, assets.stars, async (player, star) => {
+                player.hp += 5;
+                // for (const i of Array(10)) {
+                //     if (star.scale > 0) {
+                //         star.setScale(star.scale - 0.01);
+                //     } else {
+                //         star.disableBody(true, true);
+                //     }
+                //     await sleep(0.2);
+                // };
                 star.disableBody(true, true);
             }, null, this);
 
             // When asteroid collides with earth, reduce earth HP and delete asteroid
-            this.physics.add.overlap(earth, assets.asteroids, (earth, asteroid) => {
-                if (!earth.hp >= 0) {
-                    earth.hp -= 10;
-                };
+            this.physics.add.overlap(earth, assets.asteroids, async (earth, asteroid) => {
+                earth.hp -= 10;
+                // for (const i of Array(10)) {
+                //     if (asteroid.scale > 0) {
+                //         asteroid.setScale(asteroid.scale - 0.01);
+                //     } else {
+                //         asteroid.disableBody(true, true);
+                //     }
+                //     await sleep(0.2);
+                // };
                 asteroid.disableBody(true, true);
             }, null, this);
         }, null, this);
@@ -231,6 +265,12 @@
         //     player.anims.play('move', true);
         // }
 
+        // Variable Bounds
+        if (player.hp < 0) player.hp = 0;
+        if (player.hp > 100) player.hp = 100;
+        if (earth.hp < 0) earth.hp = 0;
+        if (earth.hp > 100) earth.hp = 100;
+
         // Arrow Key Rotation
         if (arrows.left.isDown || keys.A.isDown) {
             // Rotate Anti-Clockwise
@@ -255,11 +295,11 @@
         }
 
         // Player HP Tracks Player
-        assets.sharkhp.setX(player.x - (assets.sharkhp.width / 2)).setY(player.y - 50).setText('HP: ' + player.hp + '%');
+        assets.sharkhp.setX(player.x - (assets.sharkhp.width / 2)).setY(player.y - 50).setText('HP: ' + player.hp + '%').setFontFamily('Overfield');
         // Earth HP Updater
-        assets.earthhp.setX(centre.x - assets.earthhp.width / 2).setText('HP: ' + earth.hp + '%');
+        assets.earthhp.setX(centre.x - assets.earthhp.width / 2).setText('HP: ' + earth.hp + '%').setFontFamily('Overfield');
         // Elapsed Time Updater
-        assets.timer.setText(formatSecs(timerVal));
+        assets.timer.setText(formatSecs(timerVal)).setFontFamily('Overfield');
 
         // Player Death Handler
         if (player.hp <= 0) {
@@ -285,11 +325,30 @@
         game = {};
         killCanvas = true;
         clearInterval(timer);
+
+        // Store High Score
+        if (timerVal > highscore) {
+            highscore = timerVal;
+        };
     };
 
     function refresh() {
         window.location = window.location;
     };
+
+    function sleep(s) {
+        return new Promise(resolve => setTimeout(resolve, (s * 1000)));
+    };
+
+    function playBgm() {
+        let tracks = ['fanfare_dans_lusine', 'castles_wars', 'traps', 'burglar_theme'];
+        let random = Math.floor(Math.random() * (tracks.length - 1 + 1)) + 1;
+        music = game.sound.add(tracks[random - 1], {
+            volume: 0.2
+        });
+
+        music.play();
+    }
 
     function formatSecs(secs, str = false) {
         // Format Seconds into mm:ss String
@@ -302,32 +361,39 @@
         }
     };
 
-    window.addEventListener('keydown',  () => gameCanvas.focus())
+    window.addEventListener('keydown',  () => document.querySelector('#game').focus())
 </script>
 
-<div class="flex flex-row items-center justify-evenly w-screen bg-[#141931] p-10 h-screen overflow-hidden font-space">
+<div id="mainbox" class="flex flex-row items-center w-screen h-screen overflow-hidden justify-evenly font-space">
     <!-- <h1 class="text-4xl font-bold text-white rotate-180 vertical-rl">DGT - Shark Space Game</h1> -->
 
-    <div class:hidden={state != 'menu'} class="flex flex-col items-center justify-center text-white bg-blue-900 gameBox">
+    <div class:hidden={state != 'menu'} class="flex flex-col items-center justify-center p-10 text-white bg-blue-900 gameBox">
         <h1 class="text-6xl text-center">Space Shark Game</h1>
-        <button class="p-3 px-24 mt-5 text-lg bg-green-400 rounded-xl" on:click={() => loadGame()}>Play Game</button>
+        {#if highscore > 0}<h2 class="text-4xl text-center">Your high score is {formatSecs(highscore, true)}.</h2>{/if}
+
+        <div class="flex flex-row items-center justify-center w-2/3">
+            <button class="flex-1 p-3 px-24 mt-5 text-lg bg-green-600 hover:bg-green-500 rounded-xl" on:click={() => loadGame()}>Play Game</button>
+            <button class="flex-1 p-3 px-24 mt-5 ml-5 text-lg bg-green-600 hover:bg-green-500 rounded-xl" on:click={() => loadGame()}>How to Play</button>
+        </div>
     </div>
 
     {#if killCanvas != true}
     <canvas id="game" class:hidden={state != 'game'} class="gameBox" bind:this={gameCanvas}></canvas>
     {/if}
 
-    <div class:hidden={state != 'playerDeath'} class="flex flex-col items-center justify-center text-white bg-blue-900 gameBox">
+    <div class:hidden={state != 'playerDeath'} class="flex flex-col items-center justify-center p-10 text-white bg-blue-900 gameBox">
         <h1 class="text-6xl text-center">You Died!</h1>
-        <h2 class="text-4xl text-center">You defended the earth, but at great personal cost...</h2>
+        <h2 class="mb-5 text-4xl text-center">You defended the earth, but at great personal cost...</h2>
         <h2 class="text-4xl text-center">You survived for {formatSecs(timerVal, true)}.</h2>
+        <h2 class="text-4xl text-center">Your high score is {formatSecs(highscore, true)}.</h2>
         <button class="p-3 px-24 mt-5 text-lg bg-green-400 rounded-xl" on:click={() => refresh()}>Play Again</button>
     </div>
 
-    <div class:hidden={state != 'earthDeath'} class="flex flex-col items-center justify-center text-white bg-blue-900 gameBox">
+    <div class:hidden={state != 'earthDeath'} class="flex flex-col items-center justify-center p-10 text-white bg-blue-900 gameBox">
         <h1 class="text-6xl text-center">You Failed!</h1>
-        <h2 class="text-4xl text-center">You failed to defend the earth from asteroids...</h2>
+        <h2 class="mb-5 text-4xl text-center">You failed to defend the earth from asteroids...</h2>
         <h2 class="text-4xl text-center">You survived for {formatSecs(timerVal, true)}.</h2>
+        <h2 class="text-4xl text-center">Your high score is {formatSecs(highscore, true)}.</h2>
         <button class="p-3 px-24 mt-5 text-lg bg-green-400 rounded-xl" on:click={() => refresh()}>Play Again</button>
     </div>
 </div>
@@ -337,10 +403,42 @@
         writing-mode: vertical-rl;
     } */
 
+    #mainbox {
+        /* background-color: #141931; */
+        background-image: url('/assets/img/space.gif');
+        background-position: center;
+        background-repeat: no-repeat;
+        /* background-repeat: repeat; */
+        background-size: cover;
+        /* background-size: 50%; */
+    }
+
+    /* #mainbox {
+        position: relative;
+        overflow: hidden;
+    }
+
+    #mainbox::before {
+        content: "";
+        position: absolute;
+        width: 150vw;
+        height: 150vw;
+        top: -50%;
+        left: -50%;
+        z-index: -1;
+        background-image: url('/assets/img/space.png');
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        transform: rotate(-30deg);
+    } */
+
     .gameBox {
         width: 1200px;
         /* height: 675px; */
         /* height: 100%; */
         aspect-ratio: 16 / 9;
+        border-radius: 1rem;
+        @apply border-4 border-gray-400 shadow-xl;
     }
 </style>
