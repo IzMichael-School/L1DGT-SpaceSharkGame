c0ef01fa40f20864aa2939c22305b3aa6056279d
IzMichael <themichael77@outlook.com>
2022-09-18 16:21:03 +1200

    Update Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    content: ['./public/index.html', './src/**/*.svelte'],
+    content: ['./public/index.html', './src/*.svelte'],
     theme: {
         extend: {},
         fontFamily: {
